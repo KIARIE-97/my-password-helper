17c60c5ac1e6bc33acd52f72680b1eedf190924c
sarah kiarie <sarahwanjiruki1@gmail.com>
2024-06-25 11:53:11 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useCallback, useEffect, useState, useRef } from 'react'
+
+import './App.css'
+
+function App() {
+  const copyPasteToClipboard = () => {
+    console.log('copyPasteToClipboard')
+  }
+  
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
